2f26621a12c55e77baa3074e379e739b4c52646d
Arsène Rurangiza <a.rurangiza@gmail.com>
2025-06-15 07:52:06 +0200

    feat: adds streaming support

--- api/src/graymatter/api/chat/router.py
+++ api/src/graymatter/api/chat/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from .schema import ChatRequest, GenerationResponse
 from .service import OpenAIClient
@@ -7,10 +7,17 @@ router = APIRouter(prefix="/chat", tags=["chat"])
 
 
 @router.post("")
-async def generate_response(request: ChatRequest) -> GenerationResponse:
+async def completion(request: ChatRequest) -> GenerationResponse:
     client = OpenAIClient(request.model)
     if request.stream:
-        raise HTTPException(status_code=400, detail="Streaming is not implemented yet")
+        return client.stream(
+            messages=[
+                {"role": "system", "content": "You're a helpful assistant"},
+                {"role": "user", "content": request.query},
+            ],
+            temperature=request.temperature,
+            top_p=request.top_p,
+        )
     else:
         return client.complete(
             messages=[
--- api/src/graymatter/api/chat/service.py
+++ api/src/graymatter/api/chat/service.py
@@ -1,6 +1,7 @@
 import os
 
 from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
 
 from .schema import GenerationResponse
 
@@ -12,7 +13,10 @@ class OpenAIClient:
         self.model = model
 
     def complete(
-        self, messages: list[dict], temperature: float, top_p: float
+        self,
+        messages: list[ChatCompletionMessageParam],
+        temperature: float,
+        top_p: float,
     ) -> GenerationResponse:
         response = self.client.chat.completions.create(
             model=self.model,
@@ -21,8 +25,55 @@ class OpenAIClient:
             top_p=top_p,
             stream=False,
         )
+
+        if response.choices[0].message.tool_calls:
+            raise NotImplementedError("Tool calls are not available")
+
         return GenerationResponse(
             content=response.choices[0].message.content,
-            usage=response.usage,
+            usage={
+                "completion_tokens": response.usage.completion_tokens,
+                "prompt_tokens": response.usage.prompt_tokens,
+                "total_tokens": response.usage.total_tokens,
+            },
             tool_calls=response.choices[0].message.tool_calls,
+        )
+
+    def stream(
+        self,
+        messages: list[ChatCompletionMessageParam],
+        temperature: float,
+        top_p: float,
+    ) -> GenerationResponse:
+        stream_response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=temperature,
+            top_p=top_p,
+            stream=True,
+            stream_options={"include_usage": True},
+        )
+        first_chunk = next(stream_response)
+
+        full_content, usage = "", {}
+
+        if first_chunk.choices[0].delta.tool_calls:
+            raise NotImplementedError("Tool calls are not available")
+        else:
+            full_content += first_chunk.choices[0].delta.content
+
+        for chunk in stream_response:
+            if chunk.choices and (content := chunk.choices[0].delta.content):
+                full_content += content
+            elif completion_usage := chunk.usage:
+                usage = completion_usage
+
+        return GenerationResponse(
+            content=full_content,
+            usage={
+                "completion_tokens": usage.completion_tokens,
+                "prompt_tokens": usage.prompt_tokens,
+                "total_tokens": usage.total_tokens,
+            },
+            tool_calls=None,
         )
